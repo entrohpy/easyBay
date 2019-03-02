935c3d99c52c078f86187427f6a670e524c0a9da
Rohit Nema <rnema@icloud.com>
2019-03-02 02:44:32 -0800

    initial commit

--- EasyBay/ViewController.swift
+++ EasyBay/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  EasyBay
-//
-//  Created by Rohit Nema on 3/2/19.
-//  Copyright © 2019 Rohit Nema. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
