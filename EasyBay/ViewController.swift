d0b654a69b8bcbb473649e8b5d147a989bc8b188
Rohit Nema <rnema@icloud.com>
2019-03-02 02:42:25 -0800

    Initial Commit

---
+++ EasyBay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyBay
+//
+//  Created by Rohit Nema on 3/2/19.
+//  Copyright © 2019 Rohit Nema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ EasyBayTests/EasyBayTests.swift
@@ -0,0 +1,34 @@
+//
+//  EasyBayTests.swift
+//  EasyBayTests
+//
+//  Created by Rohit Nema on 3/2/19.
+//  Copyright © 2019 Rohit Nema. All rights reserved.
+//
+
+import XCTest
+@testable import EasyBay
+
+class EasyBayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
